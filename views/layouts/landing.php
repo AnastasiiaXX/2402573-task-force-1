773cffb5da4c04b0f8aca6db82aa770634f8e78e
Anastasiia <bjko.smsng@gmail.com>
2025-12-29 16:58:28 +0300

    add view for landing

---
+++ views/layouts/landing.php
@@ -0,0 +1,21 @@
+<?php
+/** @var yii\web\View $this */
+/** @var string $content */
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>TaskForce</title>
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web/css/normalize.css') ?>">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web/css/landing.css') ?>">
+</head>
+<body class="landing">
+
+<?= $content ?>
+
+<div class="overlay"></div>
+<script src="<?= Yii::getAlias('@web/js/landing.js') ?>"></script>
+</body>
+</html>
